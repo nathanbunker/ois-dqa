a8373c0172b5c2d4c92cc5076afe82ad09fffd62
nathanbunker <nathanbunker@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2011-06-30 14:48:58 +0000

    Working on Sprint 4 - DQA report

--- trunk/dqa-wicket/src/main/java/org/openimmunizationsoftware/dqa/IncomingServlet.java
+++ trunk/dqa-wicket/src/main/java/org/openimmunizationsoftware/dqa/IncomingServlet.java
@@ -53,6 +53,7 @@ public class IncomingServlet extends HttpServlet
   {
     fileImportManager = FileImportManager.getFileImportManager();
   }
+ 
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
