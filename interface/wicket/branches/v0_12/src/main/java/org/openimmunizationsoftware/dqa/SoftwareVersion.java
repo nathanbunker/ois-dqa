67f4621938bcdcfd794a1d536b2dd8b6f78a77f7
nathanbunker <nathanbunker@users.noreply.github.com>
2012-03-15 19:46:26 +0000

    Fixed issue with VFC at vaccination level

--- interface/wicket/branches/v0_12/src/main/java/org/openimmunizationsoftware/dqa/SoftwareVersion.java
+++ interface/wicket/branches/v0_12/src/main/java/org/openimmunizationsoftware/dqa/SoftwareVersion.java
@@ -3,7 +3,7 @@ package org.openimmunizationsoftware.dqa;
 public class SoftwareVersion
 {
   public static final String VENDOR = "OIS";
-  public static final String VERSION = "0.12d";
+  public static final String VERSION = "0.12.2";
   public static final String PRODUCT = "DQA";
   public static final String BINARY_ID = "";
   
