1cb368e761d7e84074fe72f0b9a982db7d110473
nathanbunker <nathanbunker@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2012-04-04 03:44:10 +0000

    branch for v1.00

--- interface/wicket/branches/v1_00/src/main/java/org/openimmunizationsoftware/dqa/SoftwareVersion.java
+++ interface/wicket/branches/v1_00/src/main/java/org/openimmunizationsoftware/dqa/SoftwareVersion.java
@@ -3,7 +3,7 @@ package org.openimmunizationsoftware.dqa;
 public class SoftwareVersion
 {
   public static final String VENDOR = "OIS";
-  public static final String VERSION = "1.0d";
+  public static final String VERSION = "1.00";
   public static final String PRODUCT = "DQA";
   public static final String BINARY_ID = "";
   
