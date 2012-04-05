3665ec49962f83aecd67c8e7c6268877f9a05ae3
nathanbunker <nathanbunker@users.noreply.github.com>
2012-04-05 12:04:56 +0000

    Updated script for HSQL to clean up database

--- database/hsql/trunk/clean.sql
+++ database/hsql/trunk/clean.sql
@@ -12,3 +12,12 @@ delete from dqa_code_received;
 delete from dqa_message_received;
 delete from dqa_potential_issue_status where profile_id >= 1200;
 
+delete from dqa_keyed_setting;
+
+INSERT INTO dqa_keyed_setting (keyed_code, object_code, object_id, keyed_value) VALUES ('validate.header.sending_facility.pfs', 'Application', 2, 'Y');
+INSERT INTO dqa_keyed_setting (keyed_code, object_code, object_id, keyed_value) VALUES ('validate.header.sending_facility.pfs', 'Application', 3, 'Y');
+INSERT INTO dqa_keyed_setting (keyed_code, object_code, object_id, keyed_value) VALUES ('validate.header.sending_facility.pfs', 'Application', 10, 'Y');
+
+INSERT INTO dqa_keyed_setting (keyed_code, object_code, object_id, keyed_value) VALUES ('validate.vaccination.facility.pfs', 'Application', 2, 'Y');
+INSERT INTO dqa_keyed_setting (keyed_code, object_code, object_id, keyed_value) VALUES ('validate.vaccination.facility.pfs', 'Application', 3, 'Y');
+INSERT INTO dqa_keyed_setting (keyed_code, object_code, object_id, keyed_value) VALUES ('validate.vaccination.facility.pfs', 'Application', 10, 'Y');
