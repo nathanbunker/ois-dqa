c393977ca34f7581bd1bc597f70fd0ea4248a90b
hillj2 <hillj2@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2012-05-08 20:25:24 +0000

    DQA web service stub

---
+++ interface/webservice/trunk/src/main/java/org/openimmunizationsoftware/dqa/service/DqaService_PortType.java
@@ -0,0 +1,12 @@
+/**
+ * DqaService_PortType.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package org.openimmunizationsoftware.dqa.service;
+
+public interface DqaService_PortType extends java.rmi.Remote {
+    public org.openimmunizationsoftware.dqa.service.SubmitMessageResultType submitMessage(org.openimmunizationsoftware.dqa.service.SubmitMessageType request) throws java.rmi.RemoteException, org.openimmunizationsoftware.dqa.service.FaultType;
+}
