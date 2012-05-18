d1b1db46c74320f01c47b8b15103b8f19e44d58b
nathanbunker <nathanbunker@users.noreply.github.com>
2012-05-18 17:41:24 +0000

    Initial import.

---
+++ interface/webservice-client/src/main/java/org/openimmunizationsoftware/dqa/service/client/FaultType.java
@@ -0,0 +1,222 @@
+/**
+ * FaultType.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package org.openimmunizationsoftware.dqa.service.client;
+
+public class FaultType  extends org.apache.axis.AxisFault  implements java.io.Serializable {
+    private java.lang.String errorCode;  // attribute
+
+    private int index;  // attribute
+
+    private int statusCode;  // attribute
+
+    private java.lang.String statusMessage;  // attribute
+
+    public FaultType() {
+    }
+
+    public FaultType(
+           java.lang.String errorCode,
+           int index,
+           int statusCode,
+           java.lang.String statusMessage) {
+        this.errorCode = errorCode;
+        this.index = index;
+        this.statusCode = statusCode;
+        this.statusMessage = statusMessage;
+    }
+
+
+    /**
+     * Gets the errorCode value for this FaultType.
+     * 
+     * @return errorCode
+     */
+    public java.lang.String getErrorCode() {
+        return errorCode;
+    }
+
+
+    /**
+     * Sets the errorCode value for this FaultType.
+     * 
+     * @param errorCode
+     */
+    public void setErrorCode(java.lang.String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+
+    /**
+     * Gets the index value for this FaultType.
+     * 
+     * @return index
+     */
+    public int getIndex() {
+        return index;
+    }
+
+
+    /**
+     * Sets the index value for this FaultType.
+     * 
+     * @param index
+     */
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+
+    /**
+     * Gets the statusCode value for this FaultType.
+     * 
+     * @return statusCode
+     */
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+
+    /**
+     * Sets the statusCode value for this FaultType.
+     * 
+     * @param statusCode
+     */
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+
+    /**
+     * Gets the statusMessage value for this FaultType.
+     * 
+     * @return statusMessage
+     */
+    public java.lang.String getStatusMessage() {
+        return statusMessage;
+    }
+
+
+    /**
+     * Sets the statusMessage value for this FaultType.
+     * 
+     * @param statusMessage
+     */
+    public void setStatusMessage(java.lang.String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof FaultType)) return false;
+        FaultType other = (FaultType) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.errorCode==null && other.getErrorCode()==null) || 
+             (this.errorCode!=null &&
+              this.errorCode.equals(other.getErrorCode()))) &&
+            this.index == other.getIndex() &&
+            this.statusCode == other.getStatusCode() &&
+            ((this.statusMessage==null && other.getStatusMessage()==null) || 
+             (this.statusMessage!=null &&
+              this.statusMessage.equals(other.getStatusMessage())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getErrorCode() != null) {
+            _hashCode += getErrorCode().hashCode();
+        }
+        _hashCode += getIndex();
+        _hashCode += getStatusCode();
+        if (getStatusMessage() != null) {
+            _hashCode += getStatusMessage().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(FaultType.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://dqaws.openimmunizationsoftware.org/dqa/schema", "FaultType"));
+        org.apache.axis.description.AttributeDesc attrField = new org.apache.axis.description.AttributeDesc();
+        attrField.setFieldName("errorCode");
+        attrField.setXmlName(new javax.xml.namespace.QName("", "errorCode"));
+        attrField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        typeDesc.addFieldDesc(attrField);
+        attrField = new org.apache.axis.description.AttributeDesc();
+        attrField.setFieldName("index");
+        attrField.setXmlName(new javax.xml.namespace.QName("", "index"));
+        attrField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        typeDesc.addFieldDesc(attrField);
+        attrField = new org.apache.axis.description.AttributeDesc();
+        attrField.setFieldName("statusCode");
+        attrField.setXmlName(new javax.xml.namespace.QName("", "statusCode"));
+        attrField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        typeDesc.addFieldDesc(attrField);
+        attrField = new org.apache.axis.description.AttributeDesc();
+        attrField.setFieldName("statusMessage");
+        attrField.setXmlName(new javax.xml.namespace.QName("", "statusMessage"));
+        attrField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        typeDesc.addFieldDesc(attrField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+
+    /**
+     * Writes the exception data to the faultDetails
+     */
+    public void writeDetails(javax.xml.namespace.QName qname, org.apache.axis.encoding.SerializationContext context) throws java.io.IOException {
+        context.serialize(qname, null, this);
+    }
+}
