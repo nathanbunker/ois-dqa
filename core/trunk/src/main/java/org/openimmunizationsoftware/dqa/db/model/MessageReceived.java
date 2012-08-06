4d692deb74b9e28f7af7b8b4b9082281e82cee5b
nathanbunker <nathanbunker@users.noreply.github.com>
2012-08-06 21:48:21 +0000

    Moving from Crystal Lightning respository

---
+++ core/trunk/src/main/java/org/openimmunizationsoftware/dqa/db/model/MessageReceived.java
@@ -0,0 +1,216 @@
+package org.openimmunizationsoftware.dqa.db.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openimmunizationsoftware.dqa.db.model.received.NextOfKin;
+import org.openimmunizationsoftware.dqa.db.model.received.Patient;
+import org.openimmunizationsoftware.dqa.db.model.received.Vaccination;
+
+public class MessageReceived implements Serializable
+{
+  
+  private static final long serialVersionUID = 1l;
+  
+  private IssueAction issueAction = null;
+  private List<IssueFound> issuesFound = new ArrayList<IssueFound>();
+  private List<NextOfKin> nextOfKins = new ArrayList<NextOfKin>();
+  private Patient patient = new Patient();
+  private SubmitterProfile profile = null;
+  private Date receivedDate = new Date();
+  private long receivedId = 0l;
+  private String requestText = null;
+  private String responseText = null;
+  private SubmitStatus submitStatus = null;
+  private Map<String, Vaccination> vaccinationMap = new HashMap<String, Vaccination>();
+  private List<Vaccination> vaccinations = new ArrayList<Vaccination>();
+  private MessageHeader messageHeader = new MessageHeader();
+  private boolean debugOn = false;
+  private boolean successfulCompletion = true;
+  private Exception exception = null;
+
+  public void setMessageHeader(MessageHeader header)
+  {
+    this.messageHeader = header;
+  }
+
+  public MessageHeader getMessageHeader()
+  {
+    return messageHeader;
+  }
+
+  public IssueAction getIssueAction()
+  {
+    return issueAction;
+  }
+
+  public List<IssueFound> getIssuesFound()
+  {
+    return issuesFound;
+  }
+
+  public List<NextOfKin> getNextOfKins()
+  {
+    return nextOfKins;
+  }
+
+  public Patient getPatient()
+  {
+    return patient;
+  }
+
+  public SubmitterProfile getProfile()
+  {
+    return profile;
+  }
+
+  public Date getReceivedDate()
+  {
+    return receivedDate;
+  }
+
+  public long getReceivedId()
+  {
+    return receivedId;
+  }
+
+  public String getRequestText()
+  {
+    return requestText;
+  }
+
+  public String getResponseText()
+  {
+    return responseText;
+  }
+
+  public SubmitStatus getSubmitStatus()
+  {
+    return submitStatus;
+  }
+
+  public Map<String, Vaccination> getVaccinationMap()
+  {
+    return vaccinationMap;
+  }
+
+  public List<Vaccination> getVaccinations()
+  {
+    return vaccinations;
+  }
+
+  public boolean hasErrors()
+  {
+    for (IssueFound issueFound : issuesFound)
+    {
+      if (issueFound.getIssueAction().equals(IssueAction.ERROR))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean hasWarns()
+  {
+    for (IssueFound issueFound : issuesFound)
+    {
+      if (issueFound.getIssueAction().equals(IssueAction.WARN))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void setIssueAction(IssueAction issueAction)
+  {
+    this.issueAction = issueAction;
+  }
+
+  public void setIssuesFound(List<IssueFound> issuesFound)
+  {
+    this.issuesFound = issuesFound;
+  }
+
+  public void setNextOfKins(List<NextOfKin> nextOfKins)
+  {
+    this.nextOfKins = nextOfKins;
+  }
+
+  public void setPatient(Patient patient)
+  {
+    this.patient = patient;
+  }
+
+  public void setProfile(SubmitterProfile profile)
+  {
+    this.profile = profile;
+  }
+
+  public void setReceivedDate(Date receivedDate)
+  {
+    this.receivedDate = receivedDate;
+  }
+
+  public void setReceivedId(long receivedId)
+  {
+    this.receivedId = receivedId;
+  }
+
+  public void setRequestText(String requestText)
+  {
+    this.requestText = requestText;
+  }
+
+  public void setResponseText(String responseText)
+  {
+    this.responseText = responseText;
+  }
+
+  public void setSubmitStatus(SubmitStatus submitStatus)
+  {
+    this.submitStatus = submitStatus;
+  }
+
+  public void setVaccinationMap(Map<String, Vaccination> vaccinationMap)
+  {
+    this.vaccinationMap = vaccinationMap;
+  }
+
+  public void setVaccinations(List<Vaccination> vaccinations)
+  {
+    this.vaccinations = vaccinations;
+  }
+  
+  public void setDebug(boolean debug)
+  {
+	this.debugOn = debug;
+  }
+
+  public boolean isDebugOn()
+  {
+	return this.debugOn;
+  }
+  public void setSuccessful(boolean success)
+  {
+    this.successfulCompletion = success;
+  }
+
+  public boolean isSuccessful()
+  {
+    return this.successfulCompletion;
+  }
+  public Exception getException()
+  {
+    return exception;
+  }
+  public void setException(Exception e)
+  {
+    this.exception = e;
+  }
+}
---
+++ core/trunk/src/main/java/org/openimmunizationsoftware/dqa/process/MessageProcessor.java
@@ -0,0 +1,175 @@
+package org.openimmunizationsoftware.dqa.process;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.openimmunizationsoftware.dqa.db.model.BatchReport;
+import org.openimmunizationsoftware.dqa.db.model.CodeReceived;
+import org.openimmunizationsoftware.dqa.db.model.CodeTable;
+import org.openimmunizationsoftware.dqa.db.model.IssueAction;
+import org.openimmunizationsoftware.dqa.db.model.MessageBatch;
+import org.openimmunizationsoftware.dqa.db.model.MessageReceived;
+import org.openimmunizationsoftware.dqa.db.model.SubmitterProfile;
+import org.openimmunizationsoftware.dqa.manager.CodesReceived;
+import org.openimmunizationsoftware.dqa.manager.MessageReceivedManager;
+import org.openimmunizationsoftware.dqa.parse.VaccinationUpdateParserHL7;
+import org.openimmunizationsoftware.dqa.quality.QualityCollector;
+import org.openimmunizationsoftware.dqa.validate.Validator;
+
+public class MessageProcessor {
+
+// set for webservice
+// results.setBatchId(qualityCollector.getMessageBatch().getBatchId());
+  
+  public static String processDebugOutput(Session session, SubmitterProfile profile, QualityCollector qualityCollector)
+  {
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter out = new PrintWriter(stringWriter);
+    if (qualityCollector != null)
+    {
+      processMessageBatch(out, qualityCollector);
+    }
+    out.print("\r");
+    out.print("\r");
+    CodesReceived cr = CodesReceived.getCodesReceived(profile, session);
+    CodesReceived masterCr = CodesReceived.getCodesReceived();
+    for (CodeTable codeTable : cr.getCodeTableList())
+    {
+      List<CodeReceived> codesReceived = cr.getCodesReceived(codeTable);
+      if (codesReceived.size() > 0)
+      {
+        out.print("-- " + padSlash(codeTable.getTableLabel() + " ", 92) + "\r");
+        out.print("\r");
+        out.print("VALUES RECEIVED\r");
+        out.print(pad("value", 20));
+        out.print(pad("label", 30));
+        out.print(pad("use instead", 20));
+        out.print(pad("status", 15));
+        out.print(pad("count", 7));
+        out.print("\r");
+        for (CodeReceived codeReceived : codesReceived)
+        {
+          out.print(pad(codeReceived.getReceivedValue(), 20));
+          out.print(pad(codeReceived.getCodeLabel(), 30));
+          if (codeReceived.getCodeValue() == null || (codeReceived.getCodeValue().equals(codeReceived.getReceivedValue())))
+          {
+            out.print(pad("", 20));
+          } else
+          {
+            out.print(pad(codeReceived.getCodeValue(), 20));
+          }
+          out.print(pad(codeReceived.getCodeStatus().getCodeLabel(), 15));
+          out.print(pad(String.valueOf(codeReceived.getReceivedCount()), 7));
+          out.print("\r");
+        }
+        codesReceived = masterCr.getCodesReceived(codeTable);
+        out.print("\r");
+        out.print("MASTER VALUE LIST\r");
+        out.print(pad("value", 20));
+        out.print(pad("label", 30));
+        out.print(pad("use instead", 20));
+        out.print(pad("status", 15));
+        out.print("\r");
+
+        for (CodeReceived codeReceived : codesReceived)
+        {
+          out.print(pad(codeReceived.getReceivedValue(), 20));
+          out.print(pad(codeReceived.getCodeLabel(), 30));
+          if (codeReceived.getCodeValue() == null || (codeReceived.getCodeValue().equals(codeReceived.getReceivedValue())))
+          {
+            out.print(pad("", 20));
+          } else
+          {
+            out.print(pad(codeReceived.getCodeValue(), 20));
+          }
+          out.print(pad(codeReceived.getCodeStatus().getCodeLabel(), 15));
+          out.print("\r");
+        }
+        out.print("\r");
+      }
+    }
+    return stringWriter.toString();
+  }
+
+
+  public static MessageReceived processMessage( boolean debugFlag, VaccinationUpdateParserHL7 parser, String sb, SubmitterProfile profile, Session session, QualityCollector qualityCollector) 
+  {
+    MessageReceived messageReceived = new MessageReceived();
+    messageReceived.setDebug(debugFlag);
+   
+    Transaction tx = session.beginTransaction();
+    try
+    {
+
+      profile.initPotentialIssueStatus(session);
+      messageReceived.setProfile(profile);
+      messageReceived.setRequestText(sb);
+      parser.createVaccinationUpdateMessage(messageReceived);
+      if (profile.isProfileStatusTest()
+          && messageReceived.getMessageHeader().getProcessingStatusCode()
+              .equals(org.openimmunizationsoftware.dqa.db.model.MessageHeader.PROCESSING_ID_DEBUGGING))
+      {
+        messageReceived.setDebug(true);
+      }
+      if (!messageReceived.hasErrors())
+      {
+        Validator validator = new Validator(profile, session);
+        validator.validateVaccinationUpdateMessage(messageReceived, null);
+      }
+      qualityCollector.registerProcessedMessage(messageReceived);
+
+      String ackMessage = parser.makeAckMessage(messageReceived);
+      messageReceived.setResponseText(ackMessage);
+      messageReceived.setIssueAction(IssueAction.ACCEPT);
+      MessageReceivedManager.saveMessageReceived(profile, messageReceived, session);
+
+      tx.commit();
+      messageReceived.setSuccessful(true);
+
+    } catch (Exception exception)
+    {
+      tx.rollback();
+      String ackMessage = "MSH|^~\\&|||||201105231008000||ACK^|201105231008000|P|2.3.1|\r" + "MSA|AE|TODO|Exception occurred: "
+          + exception.getMessage() + "|\r";
+      messageReceived.setResponseText(ackMessage);
+      messageReceived.setSuccessful(false);
+      messageReceived.setException(exception);
+    }
+    return messageReceived;
+  }
+
+  private static final String PAD = "                                                                                                          ";
+
+  private static String pad(String s, int size)
+  {
+    s += PAD;
+    return s.substring(0, size - 1) + " ";
+  }
+
+  private static final String PAD_SLASH = "-----------------------------------------------------------------------------------------------------";
+
+  private static String padSlash(String s, int size)
+  {
+    s += PAD_SLASH;
+    return s.substring(0, size - 1) + "-";
+  }
+  
+  private static void processMessageBatch(PrintWriter out, QualityCollector qualityCollector)
+  {
+    MessageBatch mb = qualityCollector.getMessageBatch();
+    BatchReport r = mb.getBatchReport();
+    out.print("\r");
+    out.print("\r");
+    out.print("Message Batch Summary: \r");
+    out.print("Message Count:       " + r.getMessageCount() + "\r");
+    out.print("Patient Count:       " + r.getMessageCount() + "\r");
+    out.print("Vaccination      \r");
+    out.print(" + Administered:     " + r.getVaccinationAdministeredCount() + "\r");
+    out.print(" + Historical:       " + r.getVaccinationHistoricalCount() + "\r");
+    out.print(" + Not Administered: " + r.getVaccinationNotAdministeredCount() + "\r");
+    out.print(" + Deleted:          " + r.getVaccinationDeleteCount() + "\r");
+  }
+}
