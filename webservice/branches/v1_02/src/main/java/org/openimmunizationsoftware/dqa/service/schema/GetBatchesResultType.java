86716e3c4c2b6b450ed8b2efb739568dc5863fc2
nathanbunker <nathanbunker@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2012-08-11 21:24:17 +0000

    branching for 1.02

---
+++ webservice/branches/v1_02/src/main/java/org/openimmunizationsoftware/dqa/service/schema/GetBatchesResultType.java
@@ -0,0 +1,128 @@
+/**
+ * GetBatchesResultType.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package org.openimmunizationsoftware.dqa.service.schema;
+
+public class GetBatchesResultType  implements java.io.Serializable {
+    private org.openimmunizationsoftware.dqa.service.schema.MessageBatchType[] messageBatchList;
+
+    public GetBatchesResultType() {
+    }
+
+    public GetBatchesResultType(
+           org.openimmunizationsoftware.dqa.service.schema.MessageBatchType[] messageBatchList) {
+           this.messageBatchList = messageBatchList;
+    }
+
+
+    /**
+     * Gets the messageBatchList value for this GetBatchesResultType.
+     * 
+     * @return messageBatchList
+     */
+    public org.openimmunizationsoftware.dqa.service.schema.MessageBatchType[] getMessageBatchList() {
+        return messageBatchList;
+    }
+
+
+    /**
+     * Sets the messageBatchList value for this GetBatchesResultType.
+     * 
+     * @param messageBatchList
+     */
+    public void setMessageBatchList(org.openimmunizationsoftware.dqa.service.schema.MessageBatchType[] messageBatchList) {
+        this.messageBatchList = messageBatchList;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof GetBatchesResultType)) return false;
+        GetBatchesResultType other = (GetBatchesResultType) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.messageBatchList==null && other.getMessageBatchList()==null) || 
+             (this.messageBatchList!=null &&
+              java.util.Arrays.equals(this.messageBatchList, other.getMessageBatchList())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getMessageBatchList() != null) {
+            for (int i=0;
+                 i<java.lang.reflect.Array.getLength(getMessageBatchList());
+                 i++) {
+                java.lang.Object obj = java.lang.reflect.Array.get(getMessageBatchList(), i);
+                if (obj != null &&
+                    !obj.getClass().isArray()) {
+                    _hashCode += obj.hashCode();
+                }
+            }
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(GetBatchesResultType.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://dqaws.openimmunizationsoftware.org/dqa/schema", "GetBatchesResultType"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("messageBatchList");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://dqaws.openimmunizationsoftware.org/dqa/schema", "messageBatchList"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://dqaws.openimmunizationsoftware.org/dqa/schema", "MessageBatchType"));
+        elemField.setNillable(false);
+        elemField.setItemQName(new javax.xml.namespace.QName("http://dqaws.openimmunizationsoftware.org/dqa/schema", "messageBatchList"));
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
---
+++ webservice/branches/v1_02/src/main/java/org/openimmunizationsoftware/dqa/service/schema/GetSubmitterProfileType.java
@@ -0,0 +1,155 @@
+/**
+ * GetSubmitterProfileType.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package org.openimmunizationsoftware.dqa.service.schema;
+
+public class GetSubmitterProfileType  implements java.io.Serializable {
+    private java.lang.String profileCode;
+
+    private java.lang.Long profileId;
+
+    public GetSubmitterProfileType() {
+    }
+
+    public GetSubmitterProfileType(
+           java.lang.String profileCode,
+           java.lang.Long profileId) {
+           this.profileCode = profileCode;
+           this.profileId = profileId;
+    }
+
+
+    /**
+     * Gets the profileCode value for this GetSubmitterProfileType.
+     * 
+     * @return profileCode
+     */
+    public java.lang.String getProfileCode() {
+        return profileCode;
+    }
+
+
+    /**
+     * Sets the profileCode value for this GetSubmitterProfileType.
+     * 
+     * @param profileCode
+     */
+    public void setProfileCode(java.lang.String profileCode) {
+        this.profileCode = profileCode;
+    }
+
+
+    /**
+     * Gets the profileId value for this GetSubmitterProfileType.
+     * 
+     * @return profileId
+     */
+    public java.lang.Long getProfileId() {
+        return profileId;
+    }
+
+
+    /**
+     * Sets the profileId value for this GetSubmitterProfileType.
+     * 
+     * @param profileId
+     */
+    public void setProfileId(java.lang.Long profileId) {
+        this.profileId = profileId;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof GetSubmitterProfileType)) return false;
+        GetSubmitterProfileType other = (GetSubmitterProfileType) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.profileCode==null && other.getProfileCode()==null) || 
+             (this.profileCode!=null &&
+              this.profileCode.equals(other.getProfileCode()))) &&
+            ((this.profileId==null && other.getProfileId()==null) || 
+             (this.profileId!=null &&
+              this.profileId.equals(other.getProfileId())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getProfileCode() != null) {
+            _hashCode += getProfileCode().hashCode();
+        }
+        if (getProfileId() != null) {
+            _hashCode += getProfileId().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(GetSubmitterProfileType.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://dqaws.openimmunizationsoftware.org/dqa/schema", "GetSubmitterProfileType"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("profileCode");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://dqaws.openimmunizationsoftware.org/dqa/schema", "profileCode"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("profileId");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://dqaws.openimmunizationsoftware.org/dqa/schema", "profileId"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "long"));
+        elemField.setNillable(true);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
