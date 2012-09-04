5f7d36a69b1650ef7016774b8b40dec4b195d5c6
nathanbunker <nathanbunker@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2012-09-04 21:48:44 +0000

    branching for v1.03

---
+++ core/branches/v1_03/src/main/java/org/openimmunizationsoftware/dqa/db/model/BatchVaccineCvx.java
@@ -0,0 +1,69 @@
+package org.openimmunizationsoftware.dqa.db.model;
+
+import java.io.Serializable;
+
+public class BatchVaccineCvx implements Serializable
+{
+  
+  private static final long serialVersionUID = 1l;
+  
+  private int batchVaccineCvxId = 0;
+  private MessageBatch messageBatch = null;
+  private VaccineCvx vaccineCvx = null;
+  private int receivedCount = 0;
+  
+  public BatchVaccineCvx()
+  {
+    // defaults
+  }
+  
+  public void inc(BatchVaccineCvx batchVaccineCvx)
+  {
+    this.receivedCount += batchVaccineCvx.getReceivedCount();
+  }
+  
+  public void incReceivedCount()
+  {
+    receivedCount++;
+  }
+  
+  public BatchVaccineCvx(VaccineCvx vaccineCvx, MessageBatch messageBatch)
+  {
+    this.vaccineCvx = vaccineCvx;
+    this.messageBatch = messageBatch;
+  }
+  
+  public int getBatchVaccineCvxId()
+  {
+    return batchVaccineCvxId;
+  }
+  public void setBatchVaccineCvxId(int batchVaccineCvxId)
+  {
+    this.batchVaccineCvxId = batchVaccineCvxId;
+  }
+  public MessageBatch getMessageBatch()
+  {
+    return messageBatch;
+  }
+  public void setMessageBatch(MessageBatch messageBatch)
+  {
+    this.messageBatch = messageBatch;
+  }
+  public VaccineCvx getVaccineCvx()
+  {
+    return vaccineCvx;
+  }
+  public void setVaccineCvx(VaccineCvx vaccineCvx)
+  {
+    this.vaccineCvx = vaccineCvx;
+  }
+  public int getReceivedCount()
+  {
+    return receivedCount;
+  }
+  public void setReceivedCount(int receivedCount)
+  {
+    this.receivedCount = receivedCount;
+  }
+
+}
