018f3bbc8b50d8aaeb35310ef352d2270328c6ce
nathanbunker <nathanbunker@users.noreply.github.com>
2012-10-08 18:31:44 +0000

    preparing v1.04

--- core/branches/v1_04/src/main/java/org/openimmunizationsoftware/dqa/SoftwareVersion.java
+++ core/branches/v1_04/src/main/java/org/openimmunizationsoftware/dqa/SoftwareVersion.java
@@ -3,7 +3,7 @@ package org.openimmunizationsoftware.dqa;
 public class SoftwareVersion
 {
   public static final String VENDOR = "OIS";
-  public static final String VERSION = "1.04dev";
+  public static final String VERSION = "1.04";
   public static final String PRODUCT = "DQA";
   public static final String BINARY_ID = "";
 }
