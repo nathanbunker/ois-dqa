aae5e41814c926fb6bbcf7760e1511c2ba110c5f
eneerge <eneerge@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2013-04-19 14:20:34 +0000

    Branching for Texas ImmTrac work

---
+++ core/branches/tx_1/src/main/java/org/openimmunizationsoftware/dqa/SoftwareVersion.java
@@ -0,0 +1,9 @@
+package org.openimmunizationsoftware.dqa;
+
+public class SoftwareVersion
+{
+  public static final String VENDOR = "OIS";
+  public static final String VERSION = "1.05dev";
+  public static final String PRODUCT = "DQA";
+  public static final String BINARY_ID = "";
+}
