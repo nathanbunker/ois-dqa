c34585a17bfe46c8d314bc369caeaf1b63a0d49e
nathanbunker <nathanbunker@users.noreply.github.com>
2013-11-26 20:30:12 +0000

    working on Oracle upgrade

--- database/oracle/trunk/upgrade-1.05.sql
+++ database/oracle/trunk/upgrade-1.05.sql
@@ -5,12 +5,12 @@ CREATE SEQUENCE dqa_remote_conn_id_sequence INCREMENT BY 1 START WITH 1;
 CREATE TABLE dqa_remote_connection
 (
   connection_id       INTEGER NOT NULL PRIMARY KEY,
-  connection_code     VARCHAR(128) NOT NULL,
-  connection_label    VARCHAR(128) NOT NULL,
-  support_center_code VARCHAR(128) NOT NULL,
-  location_to         VARCHAR(256),
-  location_from       VARCHAR(256),
-  account_name        VARCHAR(128),
+  connection_code     VARCHAR2(128) NOT NULL,
+  connection_label    VARCHAR2(128) NOT NULL,
+  support_center_code VARCHAR2(128) NOT NULL,
+  location_to         VARCHAR2(256),
+  location_from       VARCHAR2(256),
+  account_name        VARCHAR2(128),
   log_level           INTEGER NOT NULL
 );
 
@@ -22,7 +22,7 @@ CREATE TABLE dqa_remote_stat
   connection_id   INTEGER NOT NULL,
   reported_date   DATE NOT NULL,
   up_since_date   DATE NOT NULL,
-  status_label    VARCHAR(128) NOT NULL,
+  status_label    VARCHAR2(128) NOT NULL,
   attempt_count   INTEGER NOT NULL,
   sent_count      INTEGER NOT NULL,
   error_count     INTEGER NOT NULL
@@ -36,8 +36,8 @@ CREATE TABLE dqa_remote_log
   stat_id         INTEGER NOT NULL,
   reported_date   DATE NOT NULL,
   log_level       INTEGER NOT NULL,
-  issue_text      VARCHAR(1000) NOT NULL,
-  exception_trace VARCHAR(4000)
+  issue_text      VARCHAR2(1000) NOT NULL,
+  exception_trace VARCHAR2(4000)
 );
 
 INSERT INTO dqa_database_log VALUES (dqa_change_id_sequence.NEXTVAL, SYSDATE, '1.05', 'Creating dqa_remote_file table');
@@ -47,8 +47,8 @@ CREATE TABLE dqa_remote_file
   file_id         INTEGER NOT NULL PRIMARY KEY,
   stat_id         INTEGER NOT NULL,
   reported_date   DATE NOT NULL,
-  file_name       VARCHAR(256) NOT NULL,
-  status_label    VARCHAR(128) NOT NULL,
+  file_name       VARCHAR2(256) NOT NULL,
+  status_label    VARCHAR2(128) NOT NULL,
   message_count   INTEGER NOT NULL,
   sent_count      INTEGER NOT NULL,
   error_count     INTEGER NOT NULL
