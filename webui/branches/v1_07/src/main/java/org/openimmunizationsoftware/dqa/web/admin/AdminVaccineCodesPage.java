29d1d6c02143468db892bc159761241f447d4b2d
nathanbunker <nathanbunker@users.noreply.github.com>
2014-01-10 17:42:50 +0000

    branching for v1.07

---
+++ webui/branches/v1_07/src/main/java/org/openimmunizationsoftware/dqa/web/admin/AdminVaccineCodesPage.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2013 by Dandelion Software & Research, Inc (DSR)
+ * 
+ * This application was written for immunization information system (IIS) community and has
+ * been released by DSR under an Apache 2 License with the hope that this software will be used
+ * to improve Public Health.  
+ */
+package org.openimmunizationsoftware.dqa.web.admin;
+
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.openimmunizationsoftware.dqa.web.DqaBasePage;
+import org.openimmunizationsoftware.dqa.web.NavigationPanel;
+import org.openimmunizationsoftware.dqa.web.SecurePage;
+
+public class AdminVaccineCodesPage extends DqaBasePage implements SecurePage
+{
+  public AdminVaccineCodesPage() {
+    this(new PageParameters());
+  }
+
+  public AdminVaccineCodesPage(PageParameters pageParameters) {
+    super(pageParameters, NavigationPanel.ADMIN);
+    
+    add(new BookmarkablePageLink("adminCvxPageLink", AdminCvxPage.class));
+    add(new BookmarkablePageLink("adminCptPageLink", AdminCptPage.class));
+
+  }
+}
