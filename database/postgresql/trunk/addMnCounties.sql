99de88969a179ddc2771f0b053403fb55afe87c4
nathanbunker <nathanbunker@users.noreply.github.com>
2014-01-10 17:22:26 +0000

    demonstration

--- database/postgresql/trunk/addMnCounties.sql
+++ database/postgresql/trunk/addMnCounties.sql
@@ -1,1 +1,2 @@
+-- NB was here --
 INSERT INTO dqa_code_master(code_master_id, table_id, code_value, code_label, use_value, code_status) VALUES  (NEXTVAL('dqa_code_master_id_sequence'), 3, 'MN003', 'Anoka', 'ANOKA', 'V');
