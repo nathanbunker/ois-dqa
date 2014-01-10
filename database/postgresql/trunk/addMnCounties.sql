da2c2f985a8934976f720fba72c879c948769b4e
dj59 <dj59@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2014-01-10 17:14:55 +0000

    postgresql

---
+++ database/postgresql/trunk/addMnCounties.sql
@@ -0,0 +1,1 @@
+INSERT INTO dqa_code_master(code_master_id, table_id, code_value, code_label, use_value, code_status) VALUES  (NEXTVAL('dqa_code_master_id_sequence'), 3, 'MN003', 'Anoka', 'ANOKA', 'V');
