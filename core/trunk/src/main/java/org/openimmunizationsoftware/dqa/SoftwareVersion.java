ef114395472de14619b66cac669c15bfff78f053
nathanbunker <nathanbunker@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2014-09-02 19:39:15 +0000

    updated version number

--- core/trunk/src/main/java/org/openimmunizationsoftware/dqa/SoftwareVersion.java
+++ core/trunk/src/main/java/org/openimmunizationsoftware/dqa/SoftwareVersion.java
@@ -10,7 +10,7 @@ package org.openimmunizationsoftware.dqa;
 public class SoftwareVersion
 {
   public static final String VENDOR = "OIS";
-  public static final String VERSION = "1.07dev";
+  public static final String VERSION = "1.08dev";
   public static final String PRODUCT = "DQA";
   public static final String BINARY_ID = "";
 }
