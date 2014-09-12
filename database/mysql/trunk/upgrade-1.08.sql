fb9e3bb1c0a57fe19d80a98aa257b0042593bed0
nathanbunker <nathanbunker@users.noreply.github.com>
2014-09-12 12:58:51 +0000

    Added support for synchronizing profile

--- database/mysql/trunk/upgrade-1.08.sql
+++ database/mysql/trunk/upgrade-1.08.sql
@@ -319,7 +319,188 @@ CREATE TABLE dqa_submission_analysis
   analysis_content        LONGTEXT
 );
 
+INSERT INTO dqa_database_log VALUES (NULL, NOW(), '1.08', 'Adding ASC report template');
 
+INSERT INTO dqa_report_type(report_type_id, report_type_label) VALUES (5, 'IIS Data Quality');
+
+
+INSERT INTO dqa_report_template(template_id, template_label, report_type_id, report_definition, base_profile_id) VALUES(9, 'ASC IIS DQA', 5, '<dqa-scoring>
+  <section name="completeness" weight="50">
+    <section name="patient" weight="20">
+      <section name="required" weight="10">
+        <score label="Patient Id" denominator="patient count" numerator="Patient submitter id" weight="10" />
+        <score label="Birth Date" denominator="patient count" numerator="Patient birth date" weight="10" />
+        <score label="Sex" denominator="patient count" numerator="Patient gender" weight="10" />
+      </section>
+      <section name="expected" weight="6">
+        <score label="Ethnicity" denominator="patient count" numerator="Patient ethnicity" weight="10" />
+        <score label="System Creation Date" denominator="patient count" numerator="Patient system creation date" weight="10" />
+      </section>
+      <section name="recommended" weight="1">
+        <score label="Death Date" denominator="patient count" numerator="Patient death date" weight="10" />
+      </section>
+      <section name="optional" weight="0">
+        <score label="First Name" denominator="patient count" numerator="Patient name first" weight="5" />
+        <score label="Last Name" denominator="patient count" numerator="Patient name last" weight="5" />
+        <score label="Possible Test Name" denominator="patient count" numerator="Patient name may be test name" weight="-5" />
+        <score label="Possible Baby Name" denominator="patient count" numerator="Patient name may be temporary newborn name" weight="-10" />
+        <score label="Address" denominator="patient count" numerator="Patient address" weight="2">
+          <score label="Street" denominator="patient count" numerator="Patient address street" weight="5" />
+          <score label="City" denominator="patient count" numerator="Patient address city" weight="1" />
+          <score label="State" denominator="patient count" numerator="Patient address state" weight="1" />
+          <score label="Zip" denominator="patient count" numerator="Patient address zip" weight="1" />
+        </score>
+        <score label="Middle Name" denominator="patient count" numerator="Patient middle name" weight="10" />
+        <score label="Phone" denominator="patient count" numerator="Patient phone" weight="10" />
+        <score label="Mother''s Maiden" denominator="patient count" numerator="Patient mother''s maiden name" weight="10" />
+        <score label="Responsible Party" denominator="patient count" numerator="Patient guardian responsible party" weight="1">
+          <score label="First Name" denominator="patient count" numerator="Patient guardian name first" weight="4" />
+          <score label="Last Name" denominator="patient count" numerator="Patient guardian name last" weight="4" />
+          <score label="Same as Patient" denominator="patient count" numerator="Patient guardian name is same as underage patient" weight="-8" />
+          <score label="Relationship" denominator="patient count" numerator="Patient guardian relationship" weight="1" />
+        </score>
+        <score label="Race" denominator="patient count" numerator="Patient race" weight="10" />
+        <score label="Birth Indicator" denominator="patient count" numerator="Patient birth indicator" weight="10" />
+        <score label="Medicaid Id" denominator="patient count" numerator="Patient Medicaid number" weight="5" />
+        <score label="Alias" denominator="patient count" numerator="Patient alias" weight="0" />
+        <score label="Primary Language" denominator="patient count" numerator="Patient primary language" weight="0" />
+        <score label="Resp Party Address" denominator="patient count" numerator="Patient guardian address is missing" weight="0">
+          <score label="Street" denominator="patient count" numerator="Patient guardian address street" weight="0" />
+          <score label="City" denominator="patient count" numerator="Patient guardian address city" weight="0" />
+          <score label="State" denominator="patient count" numerator="Patient guardian address state" weight="0" />
+          <score label="Zip" denominator="patient count" numerator="Patient guardian address zip" weight="0" />
+        </score>
+        <score label="Resp Party Phone" denominator="patient count" numerator="Patient guardian phone" weight="0" />
+        <score label="Address County" denominator="patient count" numerator="Patient address county" weight="0" />
+        <score label="Financial Class" denominator="patient count" numerator="Patient VFC status" weight="0" />
+        <score label="Registry Status" denominator="patient count" numerator="Patient registry status" weight="0" />
+        <score label="Primary Physician" denominator="patient count" numerator="Patient primary physician id" weight="0">
+          <score label="Name" denominator="patient count" numerator="Patient primary physician name" weight="0" />
+        </score>
+        <score label="Primary Facility" denominator="patient count" numerator="Patient primary facility id" weight="0">
+          <score label="Name" denominator="patient count" numerator="Patient primary facility name" weight="0" />
+        </score>
+        <score label="Patient Registry Id" denominator="patient count" numerator="Patient registry id" weight="0" />
+        <score label="Protection Indicator" denominator="patient count" numerator="Patient protection indicator" weight="0" />
+        <score label="Publicity Indicator" denominator="patient count" numerator="Patient publicity code" weight="0" />
+      </section>
+    </section>
+    <section name="vaccination" weight="20">
+      <section name="required" weight="10">
+        <score label="Vaccination Date" denominator="vaccination count" numerator="Vaccination admin date" weight="40" />
+        <score label="Vaccination Code" denominator="vaccination count" numerator="Vaccination admin code" weight="40">
+          <score label="Not Specific" denominator="vaccination admin count" numerator="Vaccination admin code is not specific" weight="-10" />
+          <score label="Not Vaccine" denominator="vaccination count" numerator="Vaccination admin code is not vaccine" weight="-40" />
+          <score label="Valued as Unknown" denominator="vaccination count" numerator="Vaccination admin code is valued as unknown" weight="-40" />
+        </score>
+        <score label="Information Source" denominator="vaccination count" numerator="Vaccination information source" weight="40">
+          <score label="May be Historical" denominator="vaccination count" numerator="Vaccination information source is administered but appears to historical"
+            weight="-10" />
+          <score label="May be Administered" denominator="vaccination count" numerator="Vaccination information source is historical but appears to be administered"
+            weight="-10" />
+        </score>
+      </section>
+      <section name="expected" weight="6">
+        <score label="Manufacturer" denominator="vaccination admin count" numerator="Vaccination manufacturer code" weight="20" />
+        <score label="Vaccine Validity" denominator="vaccination count" numerator="Vaccination validity" weight="20" />
+        <score label="Facility Id" denominator="vaccination admin count" numerator="Vaccination facility id" weight="20" />
+        <score label="Body Route" denominator="vaccination admin count" numerator="Vaccination body route" weight="20" />
+        <score label="Body Site" denominator="vaccination admin count" numerator="Vaccination body site" weight="20" />
+        <score label="System Entry Date" denominator="vaccination count" numerator="Vaccination system entry time" weight="5">
+          <score label="In Future" denominator="vaccination count" numerator="Vaccination system entry time is in future" weight="-5" />
+        </score>
+      </section>
+      <section name="recommended" weight="4">
+        <score label="Trade Name" denominator="vaccination admin count" numerator="Vaccination trade name" weight="20" />
+        <score label="Facility Type" denominator="vaccination admin count" numerator="Vaccination facility type" weight="20" />
+      </section>
+      <section name="optional" weight="0">
+        <score label="Lot Number" denominator="vaccination admin count" numerator="Vaccination lot number" weight="20" />
+        <score label="Admin Amount" denominator="vaccination admin count" numerator="Vaccination administered amount" weight="10">
+          <score label="Missing Units" denominator="vaccination admin count" numerator="Vaccination administered unit is missing" weight="-2" />
+        </score>
+        <score label="Ordered By" denominator="vaccination admin count" numerator="Vaccination ordered by" weight="0" />
+        <score label="Entered By" denominator="vaccination count" numerator="Vaccination recorded by" weight="0" />
+        <score label="Refusal Reason" denominator="vaccination count" numerator="Vaccination refusal reason" weight="0" />
+        <score label="Lot Expiration Date" denominator="vaccination admin count" numerator="Vaccination lot expiration date" weight="0" />
+        <score label="CVX Code" denominator="vaccination count" numerator="Vaccination CVX code" weight="20" />
+        <score label="CPT Code" denominator="vaccination count" numerator="Vaccination CPT code" weight="0" />
+        <score label="Action Code" denominator="vaccination count" numerator="Vaccination action code" weight="10" />
+        <score label="Given By Id" denominator="vaccination admin count" numerator="Vaccination given by" weight="10" />
+        <score label="Vaccination Id" denominator="vaccination count" numerator="Vaccination id" weight="10" />
+        <score label="Completion Status" denominator="vaccination admin count" numerator="Vaccination completion status" weight="5" />
+      </section>
+    </section>
+    <section name="vaccineGroup" weight="10">
+      <section name="expected" weight="2">
+        <section name="DTAP" weight="1" />
+        <section name="HepB" weight="1" />
+        <section name="POLIO" weight="1" />
+        <section name="HIB" weight="1" />
+        <section name="HepA" weight="1" />
+        <section name="FLU" weight="1" />
+        <section name="MMR" weight="1" />
+        <section name="VARICELLA" weight="1" />
+        <section name="PneumoPCV" weight="1" />
+        <section name="Td" weight="1" />
+      </section>
+      <section name="recommended" weight="1">
+      </section>
+      <section name="optional" weight="0">
+      </section>
+      <section name="unexpected" weight="-2">
+      </section>
+    </section>
+  </section>
+  <section name="quality" weight="40">
+    <section name="errors" weight="28">
+    </section>
+    <section name="warnings" weight="12">
+    </section>
+  </section>
+  <section name="timeliness" weight="10">
+    <section name="early" weight="10" days="2">
+    </section>
+    <section name="onTime" weight="9" days="7">
+    </section>
+    <section name="late" weight="7" days="30">
+    </section>
+    <section name="veryLate" weight="5" days="60">
+    </section>
+    <section name="oldData" weight="0">
+    </section>
+  </section>
+</dqa-scoring>
+', 259);
+
+INSERT INTO dqa_submitter_profile (profile_id, profile_code, profile_label, profile_status, org_id, data_format, transfer_priority, access_key, template_id) VALUES (259, 'TEMPLATE:ASC IIS DQA', 'ASC IIS DQA', 'Template', 1, 'HL7v2', 'Normal', 'template', 9);
+
+INSERT INTO dqa_application (application_id, application_label, application_type, run_this) VALUES (15, 'ASC', 'Test', 'N');
+UPDATE dqa_application SET primary_template_id = 9 WHERE application_id = 15;
+
+
+INSERT INTO dqa_database_log VALUES (NULL, NOW(), '1.08', 'Add new counts to the report');
+ALTER TABLE dqa_batch_report ADD COLUMN (vacc_refusal_count INT(11) NOT NULL DEFAULT 0);
+
+INSERT INTO dqa_database_log VALUES (NULL, NOW(), '1.08', 'Update the labels for a select set of potential issues');
+
+UPDATE dqa_potential_issue SET default_action_code = 'W', change_priority='May', target_object='Observation', target_field='identifier code', issue_type='is deprecated', field_value='', report_denominator='Vaccination Count', table_id = 43, hl7_reference = 'OBX-3', hl7_error_code = '102', app_error_code = NULL WHERE issue_id='475';
+UPDATE dqa_potential_issue SET default_action_code = 'S', change_priority='May', target_object='Observation', target_field='identifier code', issue_type='is ignored', field_value='', report_denominator='Vaccination Count', table_id = 43, hl7_reference = 'OBX-3', hl7_error_code = '102', app_error_code = NULL WHERE issue_id='476';
+UPDATE dqa_potential_issue SET default_action_code = 'W', change_priority='May', target_object='Observation', target_field='identifier code', issue_type='is invalid', field_value='', report_denominator='Vaccination Count', table_id = 43, hl7_reference = 'OBX-3', hl7_error_code = '102', app_error_code = NULL WHERE issue_id='477';
+UPDATE dqa_potential_issue SET default_action_code = 'W', change_priority='May', target_object='Observation', target_field='identifier code', issue_type='is missing', field_value='', report_denominator='Vaccination Count', table_id = 43, hl7_reference = 'OBX-3', hl7_error_code = '101', app_error_code = NULL WHERE issue_id='478';
+UPDATE dqa_potential_issue SET default_action_code = 'W', change_priority='May', target_object='Observation', target_field='identifier code', issue_type='is unrecognized', field_value='', report_denominator='Vaccination Count', table_id = 43, hl7_reference = 'OBX-3', hl7_error_code = '103', app_error_code = NULL WHERE issue_id='479';
+UPDATE dqa_potential_issue SET default_action_code = 'W', change_priority='May', target_object='Observation', target_field='value', issue_type='is missing', field_value='', report_denominator='Vaccination Count', table_id = NULL, hl7_reference = 'OBX-5', hl7_error_code = '101', app_error_code = NULL WHERE issue_id='480';
+
+
+INSERT INTO dqa_report_template(template_id, template_label, report_type_id, base_profile_id) VALUES(0, 'No Template', 1, 1);
+
+INSERT INTO dqa_database_log VALUES (NULL, NOW(), '1.08', 'Dropping group_status from dqa_database_log, old data not used anymore');
+
+ALTER TABLE dqa_vaccine_group DROP group_status;
+
+INSERT INTO dqa_database_log VALUES (NULL, NOW(), '1.08', 'Increasing password field to accommodate hashed password');
+
+ALTER TABLE dqa_user_account MODIFY password  VARCHAR(250);
 -- not run yet
 
 INSERT INTO dqa_database_log VALUES (NULL, NOW(), '1.08', 'Finished upgrading');
