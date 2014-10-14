9b19d3dcb73877f4fe490d8631aa2c29b8d25a79
dj59 <dj59@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2014-10-14 20:01:09 +0000

    reverse what I did

--- core/branches/MN_v1.08/tx_1/src/main/java/org/openimmunizationsoftware/dqa/manager/PotentialIssueManager.java
+++
@@ -1,9 +0,0 @@
-package org.openimmunizationsoftware.dqa.manager;
-
-import org.openimmunizationsoftware.dqa.db.model.PotentialIssue;
-
-public class PotentialIssueManager
-{
-  public static final PotentialIssue GENERAL_CONFIGURATION_EXCEPTION = new PotentialIssue();
-
-}
