bc5fc8f5af23e7add7ba871a52f4412afefccda7
nathanbunker <nathanbunker@users.noreply.github.com>
2014-10-16 14:57:34 +0000

    Finished MySQL upgrade scripts

--- database/mysql/trunk/create-user.sql
+++ database/mysql/trunk/create-user.sql
@@ -2,7 +2,7 @@ CREATE DATABASE dqa;
 
 USE dqa;
 
-CREATE USER 'dqa_web'@'localhost' IDENTIFIED BY 'SharkBaitHooHaHa';
+CREATE USER 'dqa_web'@'localhost' IDENTIFIED BY 'changeme';
 
 GRANT ALL PRIVILEGES ON dqa.* TO 'dqa_web'@'localhost' WITH GRANT OPTION;
 
