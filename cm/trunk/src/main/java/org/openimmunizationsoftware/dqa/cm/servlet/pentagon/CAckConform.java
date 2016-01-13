573e2dd2cf04631cb380a2072c0f2e3986b4e5a3
larsonet9 <larsonet9>
2016-01-13 19:36:58 +0000

    Continued Documentation in the Confidence Test Area

--- cm/trunk/src/main/java/org/openimmunizationsoftware/dqa/cm/servlet/pentagon/CAckConform.java
+++ cm/trunk/src/main/java/org/openimmunizationsoftware/dqa/cm/servlet/pentagon/CAckConform.java
@@ -25,11 +25,17 @@ public class CAckConform extends PentagonBoxHelper
   public void printOverview(PrintWriter out, Session dataSession, PentagonReport pentagonReport, HttpSession webSession, UserSession userSession)
   {
     out.println("<p class=\"pentagon\">Acknowledgment messages returned should conform to the CDC Implementation Guide and HL7 requirements. "
-        + "Non-conformant messages may be incorrectly read by the testing system. "
+        + "Non-conformant messages may be incorrectly read and understood by submitting systems including this testing system. "
         + "Confidence in the results of this report increases when acknowledgments are properly formatted. "
-        + "Conformance is verified by NIST software. Please note that this section measures a similar concept to "
-        + "Update Conformance: Acks Conform, but with a different purpose and scoring metric. In this section "
-        + "each message is expected to return a conforming ACK without any errors. </p> ");
+        + "Conformance is verified by " + createExternalLink("http://hl7v2-iz-r1.5-testing.nist.gov/iztool/#/home", "NIST Immunization Test Suite") + ".</p>"
+        + "<p class=\"pentagon\">The purpose of this section is to measure the ACK message for conformance. Two distinct set of test cases are used for this purpose."
+        + "<ol class=\"pentagon\"><li>VXU messages most IIS accept and result in \"positive\" ACK messages.</li> "
+        + "<li>VXU messages with intentional errors most IIS reject and result in \"negative\" ACK messages.</li></ol></p>"
+        + "<p class=\"pentagon\">Despite distinct differences in the two types test cases, this section does not measure if an IIS accepted a good message or reject a bad message."
+        + "It merely measures the ACK message against the standard via the NIST Immunization Test Suite. "
+        + "Acceptance of good messages and rejection of bad messages are measured in other sections.</p>"
+        + "<p class=\"pentagon\">Please note that this section measures a similar concept to "
+        + "Update Conformance below, but with a different purpose and scoring metric and test cases.</p> ");
   }
 
   @Override
@@ -61,20 +67,28 @@ public class CAckConform extends PentagonBoxHelper
       }
     }
 
-    if (countTotal == 0)
+    if (countTotal < testMessageList.size())
     {
-      out.println("<h4 class=\"pentagon\">Problem Validating for Conformance</h4>");
-      out.println("<p class=\"pentagon\">A score can not be calculated because tests were not validated for conformance. </p>");
-      return;
-    } else if (countTotal < testMessageList.size())
-    {
-      out.println("<h4 class=\"pentagon\">Problem Validating for Conformance</h4>");
-      out.println("<p class=\"pentagon\">" + (testMessageList.size() - countTotal) + "/" + testMessageList.size()
-          + " were not validated for conformance.</p>");
-      return;
+      out.println("<h4 class=\"pentagon\">Not Tested - Problem Validating for Conformance</h4>");
+      out.println("<p class=\"pentagon\">The following test cases incurred problems when validating.<br>Click on each test case for further details.</p>");
+      out.println("<table class=\"pentagon\">");
+      out.println("  <tr class=\"pentagon\">");
+      out.println("    <th class=\"pentagon\">Test Case</th>");
+      out.println("  </tr>");
+      for (TestMessage testMessage : testMessageList)
+      {
+        if (testMessage.getResultAckConformance() == null ||
+            testMessage.getResultAckConformance().equals(RecordServletInterface.VALUE_RESULT_ACK_CONFORMANCE_NOT_RUN))
+        {
+          out.println("  <tr class=\"pentagon\">");
+          out.println("    <td class=\"pentagon\">" + createAjaxLink(testMessage) + "</td>");
+          out.println("  </tr>");
+        }
+      }
+      out.println("</table><br><br>");
     }
 
-    if (pentagonBox.getReportScore() < 100)
+    if (pentagonBox.getReportScore() < 100 && countTotal > 0)
     {
       out.println("<h4 class=\"pentagon\">Fail - ACK Message Conformance Problems</h4>");
       out.println("<p class=\"pentagon\">The following test cases had ACK messages which failed NIST 2015 Conformance."
@@ -98,6 +112,7 @@ public class CAckConform extends PentagonBoxHelper
       }
       out.println("</table><br><br>");
     }
+    
     if (countOk > 0)
     {
       out.println("<h4 class=\"pentagon\">Pass - Conformant ACK Messages</h4>");
@@ -127,10 +142,9 @@ public class CAckConform extends PentagonBoxHelper
   @Override
   public void printCalculation(PrintWriter out, Session dataSession, PentagonReport pentagonReport, HttpSession webSession, UserSession userSession)
   {
-    out.println("<p class=\"pentagon\">Each acknowledgement message returned for Good and Bad messages is checked for conformance "
-        + "against the National Institute of Standards and Technology (NIST) immunization validation service. "
-        + "Each message that has has not error conformance identified are included in the passing list. "
-        + "The score final score for this section is the percentage of passing divided by the total number of messages.  </p>");
+    out.println("<p class=\"pentagon\">Each ACK message returned is checked for conformance "
+        + "against the National Institute of Standards and Technology (NIST) Immunization Test Suite. "
+        + "The final score for this section is the number of conformant ACK messages divided by the total number of test cases.</p>" );
   }
 
   @Override
@@ -171,7 +185,7 @@ public class CAckConform extends PentagonBoxHelper
   @Override
   public void printImprove(PrintWriter out, Session dataSession, PentagonReport pentagonReport, HttpSession webSession, UserSession userSession)
   {
-    out.println("<p class=\"pentagon\">Update the IIS ACK message so that it conforms to the CDC Guide. "
-        + "Focus on the errors that appear the most often. Be sure to verify the response messages using the " + "NIST test site. </p>");
+    out.println("<p class=\"pentagon\">Updating the ACK message so that it conforms to the CDC Guide will help improve confidence in understanding if an IIS accepted a VXU message. "
+        + "Focus on the ACK conformance problems that appear the most often first. Be sure to verify the ACK messages using the " + createExternalLink("http://hl7v2-iz-r1.5-testing.nist.gov/iztool/#/home", "NIST Immunization Test Suite") +".</p>");
   }
 }
